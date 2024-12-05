bdc3fb275a845b9212395f0c33f09d4c7400ed5b
rix71 <enriko.siht11@gmail.com>
2024-12-05 12:33:18 +0200

    Day 5

---
+++ aoc-2024-cpp/day05/day05.cpp
@@ -0,0 +1,132 @@
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstddef>
+#include <filesystem>
+#include <fstream>
+#include <map>
+#include <ranges>
+#include <regex>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+#include <fmt/base.h>
+#include <fmt/ranges.h>
+#include <flux.hpp>
+#include <flux/op/inplace_reverse.hpp>
+
+namespace sr = std::ranges;
+namespace sv = std::views;
+namespace fs = std::filesystem;
+
+using fmt::print;
+using fmt::println;
+
+using update_t = std::vector<int>;
+using rules_t = std::map<int, std::vector<int>>;
+
+auto read_input(fs::path const& file_name) -> std::vector<std::string> {
+  std::fstream infile(fs::path{BASE_PATH} / file_name);
+  std::vector<std::string> lines;
+
+  std::string line;
+  while (std::getline(infile, line)) {
+    lines.push_back(line);
+  }
+  infile.close();
+  return lines;
+}
+
+auto get_middle_page(update_t const& update) -> int {
+  return update[update.size() / 2];
+};
+
+auto is_correct_order(update_t const& update, rules_t const& rules) -> bool {
+  for (auto [idx, page] : update | sv::enumerate) {
+    if (!rules.contains(page)) {
+      if (idx != 0) {
+        return false;
+      } else {
+        continue;
+      }
+    }
+    // All the pages that must be BEFORE this page
+    auto page_rules = rules.at(page);
+    auto pages_before = update | sv::take(idx);
+    for (auto page_before : pages_before) {
+      if (!sr::contains(page_rules, page_before)) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+auto sort_pages(update_t update, rules_t const& rules) {
+  sr::sort(update, [&rules](auto lhs, auto rhs) {
+    if (!rules.contains(rhs)) {
+      return false;
+    }
+    return sr::contains(rules.at(rhs), lhs);
+  });
+  return update;
+}
+
+void part1(std::vector<update_t> const& updates, rules_t const& rules) {
+  auto result = flux::ref(updates)
+                    .filter([&rules](auto update) {
+                      return is_correct_order(update, rules);
+                    })
+                    .map([](auto update) { return get_middle_page(update); })
+                    .sum();
+
+  println("{}", result);
+}
+
+void part2(std::vector<update_t> const& updates, rules_t const& rules) {
+  auto result = flux::ref(updates)
+                    .filter([&rules](auto update) {
+                      return !is_correct_order(update, rules);
+                    })
+                    .map([&rules](auto update) {
+                      return sort_pages(std::move(update), rules);
+                    })
+                    .map([](auto update) { return get_middle_page(update); })
+                    .sum();
+
+  println("{}", result);
+}
+
+int main() {
+  using namespace std::literals;
+  auto lines = read_input("day05/in.txt");
+
+  auto updates = std::vector<update_t>{};
+  auto rules = rules_t{};
+
+  bool reading_rules = true;
+  for (auto const& line : lines) {
+    if (reading_rules) {
+      if (line == "") {
+        reading_rules = false;
+        continue;
+      }
+
+      auto rule = flux::split_string(std::string_view{line}, "|"sv)
+                      .map([](auto s) { return std::stoi(std::string(s)); })
+                      .to<std::vector>();
+      rules[rule[1]].push_back(rule[0]);
+
+    } else {
+      auto update = flux::split_string(std::string_view{line}, ","sv)
+                        .map([](auto s) { return std::stoi(std::string(s)); })
+                        .to<update_t>();
+      updates.push_back(std::move(update));
+    }
+  }
+
+  part1(updates, rules);
+  part2(updates, rules);
+}
